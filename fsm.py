5fdf8519163c15c472519ce5365f3916fda888eb
F74061080 <F74061080@gs.ncku.edu.tw>
2019-11-29 22:44:20 +0800

    fffff

--- fsm.py
+++ fsm.py
@@ -37,7 +37,7 @@ class TocMachine(GraphMachine):
         message = TemplateSendMessage(
             alt_text='Buttons template',
             template=ButtonsTemplate(
-                thumbnail_image_url='S__26632198.jpg',
+                #thumbnail_image_url='S__26632198.jpg',
                 title='Menu',
                 text='Please select',
                 actions=[
